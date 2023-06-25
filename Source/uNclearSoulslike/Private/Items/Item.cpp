d4f251136e71bcfe5ad853be5a4da7164a13a465
uNclearPhysicist <80786683+uNclearPhysicist@users.noreply.github.com>
2023-06-25 20:15:34 +0100

    Drawing Debug Lines

--- Source/uNclearSoulslike/Private/Items/Item.cpp
+++ Source/uNclearSoulslike/Private/Items/Item.cpp
@@ -18,34 +18,18 @@ AItem::AItem()
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-
-	UE_LOG(LogTemp, Warning, TEXT("BeginPlay called!"))
-
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("Item OnScreen Message!"));
-	}
 	
 	UWorld* World = GetWorld();
 	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
+	
 	DRAW_SPHERE(Location)
+	DRAW_LINE(Location, Location+Forward*100.f)
 }
 
 // Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	//UE_LOG(LogTemp, Warning, TEXT("DeltaTime: %f"), DeltaTime)
-
-	if (GEngine)
-	{
-		FString Name = GetName();
-		FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
-		GEngine->AddOnScreenDebugMessage(2, 60.f, FColor::Cyan, Message);
-		
-		//UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name)
-	}
-
 }
 
