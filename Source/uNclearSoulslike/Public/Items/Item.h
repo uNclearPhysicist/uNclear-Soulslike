cde45cd635f31c9fcb2fc54750250e885d82c204
uNclearPhysicist <80786683+uNclearPhysicist@users.noreply.github.com>
2023-06-26 14:15:55 +0100

    Visible but not Editable

--- Source/uNclearSoulslike/Public/Items/Item.h
+++ Source/uNclearSoulslike/Public/Items/Item.h
@@ -20,6 +20,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 private:
+	UPROPERTY(VisibleAnywhere)
 	float RunningTime;
 	
 	UPROPERTY(EditAnywhere)
