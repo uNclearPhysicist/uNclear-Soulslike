d176b5d14433576105e6ac7338a99b2ff5c73ebd
uNclearPhysicist <80786683+uNclearPhysicist@users.noreply.github.com>
2023-06-27 20:44:48 +0100

    Two-Dimensional Input

--- Source/uNclearSoulslike/Private/Pawns/Bird.cpp
+++ Source/uNclearSoulslike/Private/Pawns/Bird.cpp
@@ -35,6 +35,11 @@ ABird::ABird()
 	// Set up Camera Component
 	ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("ViewCamera"));
 	ViewCamera->SetupAttachment(SpringArm);
+
+	// Use Controller Rotation
+	bUseControllerRotationPitch = true;
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationRoll = false;
 	
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
@@ -64,6 +69,16 @@ void ABird::Move(const FInputActionValue& Value)
 	}
 }
 
+void ABird::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisValue = Value.Get<FVector2D>();
+	if (GetController())
+	{
+		AddControllerYawInput(LookAxisValue.X);
+		AddControllerPitchInput(LookAxisValue.Y);
+	}
+}
+
 // Called every frame
 void ABird::Tick(float DeltaTime)
 {
@@ -79,6 +94,7 @@ void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
 	{
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABird::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABird::Look);
 	}
 }
 
