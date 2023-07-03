17cae84ada9a9432dc14239ca04e9c314dde36df
uNclearPhysicist <80786683+uNclearPhysicist@users.noreply.github.com>
2023-07-03 16:45:25 +0100

    Attaching the Sword

--- Source/uNclearSoulslike/Private/Items/Weapons/Weapon.cpp
+++ Source/uNclearSoulslike/Private/Items/Weapons/Weapon.cpp
@@ -2,11 +2,19 @@
 
 
 #include "Items/Weapons/Weapon.h"
+#include "Characters/uNclearCharacter.h"
 
 void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	AuNclearCharacter* uNclearCharacter = Cast<AuNclearCharacter>(OtherActor);
+	if (uNclearCharacter)
+	{
+		FAttachmentTransformRules TransformRules(EAttachmentRule::SnapToTarget, true);
+		ItemMesh->AttachToComponent(uNclearCharacter->GetMesh(), TransformRules, FName("RightHandSocket"));
+	}
 }
 
 void AWeapon::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
