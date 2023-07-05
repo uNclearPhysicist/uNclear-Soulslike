6be04ada38c5fe1775bcb73d8d9bbb580e6dc266
uNclearPhysicist <80786683+uNclearPhysicist@users.noreply.github.com>
2023-07-05 18:14:46 +0100

    Box Tracing

--- Source/uNclearSoulslike/Public/Items/Weapons/Weapon.h
+++ Source/uNclearSoulslike/Public/Items/Weapons/Weapon.h
@@ -22,11 +22,22 @@ public:
 	void AttachMeshToSocket(USceneComponent* InParent, FName InSocketName);
 	void Equip(USceneComponent* InParent, FName InSocketName);
 protected:
+	virtual void BeginPlay() override;
     virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
     virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+
+	UFUNCTION()
+	void OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 private:
 	UPROPERTY(EditAnywhere, Category = "Weapon Properties")
 	USoundBase* EquipSound;
+	
 	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
 	UBoxComponent* WeaponBox;
+	
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* BoxTraceStart;
+	
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* BoxTraceEnd;
 };
