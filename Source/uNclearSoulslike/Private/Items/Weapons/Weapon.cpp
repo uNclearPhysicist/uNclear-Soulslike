6ab84e8b56eb7e364bc3a418bb4ae616288457c6
uNclearPhysicist <80786683+uNclearPhysicist@users.noreply.github.com>
2023-07-08 18:48:36 +0100

    Creating Fields with Weapons

--- Source/uNclearSoulslike/Private/Items/Weapons/Weapon.cpp
+++ Source/uNclearSoulslike/Private/Items/Weapons/Weapon.cpp
@@ -103,5 +103,7 @@ void AWeapon::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* Oth
 			HitInterface->GetHit(BoxHit.ImpactPoint);
 		}
 		IgnoreActors.AddUnique(BoxHit.GetActor());
+
+		CreateFields(BoxHit.ImpactPoint);
 	}
 }
