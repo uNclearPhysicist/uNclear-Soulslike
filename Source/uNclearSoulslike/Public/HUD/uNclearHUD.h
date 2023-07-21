d601e728a12b1b3d56bd63c39bd642e673f040ab
uNclearPhysicist <80786683+uNclearPhysicist@users.noreply.github.com>
2023-07-21 15:22:20 +0100

    HUD

---
+++ Source/uNclearSoulslike/Public/HUD/uNclearHUD.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "uNclearHUD.generated.h"
+
+class UuNclearOverlay;
+
+/**
+ * 
+ */
+UCLASS()
+class UNCLEARSOULSLIKE_API AuNclearHUD : public AHUD
+{
+	GENERATED_BODY()
+
+protected:
+	
+	virtual void BeginPlay() override;
+	
+private:
+	
+	UPROPERTY(EditDefaultsOnly, Category = uNclear)
+	TSubclassOf<UuNclearOverlay> uNclearOverlayClass;
+
+	UPROPERTY()
+	UuNclearOverlay* uNclearOverlay;
+};
